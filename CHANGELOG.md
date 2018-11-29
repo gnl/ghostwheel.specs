627574f5d152f5dd8313c1049eb9bc730d8bf55d
George Lipov <gnl@lipov.net>
2018-11-29 10:55:38 +0100

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Change Log
+All notable changes to this project will be documented in this file. This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
+
+## 0.3.0 – 2018-12-01 – Initial release
+
---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject gnl/ghostwheel.specs "0.3.0-SNAPSHOT"
+  :description "A collection of third-party library specs for Clojure written in Ghostwheel's gspec syntax"
+  :url "https://github.com/gnl/ghostwheel.specs"
+  :scm {:name "git"
+        :url  "https://github.com/gnl/ghostwheel.specs"}
+  :license {:name "Eclipse Public License"
+            :url  "https://choosealicense.com/licenses/epl-2.0/"}
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
+  :dependencies [[gnl/ghostwheel "0.3.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.10.439"]
+                                  [org.clojure/clojure "1.9.0"]]}}
+  :source-paths ["src"]
+  :clean-targets ^{:protect false} ["target" "resources"])
