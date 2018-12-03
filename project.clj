49d5f1a9f8a3d234ed216d7a64597ddadbd36166
George Lipov <gnl@lipov.net>
2018-12-03 02:55:13 +0100

    Synchronise Ghostwheel module versions

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gnl/ghostwheel.specs "0.3.3-SNAPSHOT"
+(defproject gnl/ghostwheel.specs "0.3.5-SNAPSHOT"
   :description "A collection of third-party library specs for Clojure written in Ghostwheel's gspec syntax"
   :url "https://github.com/gnl/ghostwheel.specs"
   :scm {:name "git"
@@ -7,7 +7,7 @@
             :url  "https://choosealicense.com/licenses/epl-2.0/"}
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
-  :dependencies [[gnl/ghostwheel "0.3.4"]]
+  :dependencies [[gnl/ghostwheel "0.3.5"]]
   :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.10.439"]
                                   [org.clojure/clojure "1.9.0"]]}}
   :source-paths ["src"]
