ab8fb3018ef703bdc6c0d08c67372b02d31eb4c0
George Lipov <gnl@lipov.net>
2019-03-15 17:25:43 +0100

    Bump versions

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gnl/ghostwheel.specs "0.3.6-SNAPSHOT"
+(defproject gnl/ghostwheel.specs "0.3.6"
   :description "A collection of third-party library specs for Clojure written in Ghostwheel's gspec syntax"
   :url "https://github.com/gnl/ghostwheel.specs"
   :scm {:name "git"
@@ -7,8 +7,8 @@
             :url  "https://choosealicense.com/licenses/epl-2.0/"}
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
-  :dependencies [[gnl/ghostwheel "0.3.5"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.10.439"]
-                                  [org.clojure/clojure "1.9.0"]]}}
+  :dependencies [[gnl/ghostwheel "0.3.6"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.10.520"]
+                                  [org.clojure/clojure "1.10.0"]]}}
   :source-paths ["src"]
   :clean-targets ^{:protect false} ["target" "resources"])
